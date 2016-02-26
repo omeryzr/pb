023ce70b754628e464bdfa5c4c604914eaba0642
omeryzr <omeryzr_@hotmail.com>
2016-02-26 22:59:05 +0200

    create basic template

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Ömer on 24.2.2016.
+ */
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('nav').addClass('shrink');
+    } else {
+        $('nav').removeClass('shrink');
+    }
+});
